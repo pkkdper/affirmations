d753f598579e0b172d0f0cc2cb89b3f9dd4d26cd
oliwia <oliwia.giercza@gmail.com>
2023-05-25 18:32:53 -0500

    created list_item.xml which is a layout for items in the recycleview

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = DataSource().loadAffirmations().size.toString()
+
     }
 }
