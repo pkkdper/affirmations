79998cd1ebe6076a6d8c1bfd97bdbdf942baf4e5
oliwia <oliwia.giercza@gmail.com>
2023-05-29 10:47:12 -0500

    starting with instrumentation tests

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,16 @@
+package com.example.affirmations
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+class AffirmationsListTests {
+    @RunWith(AndroidJUnit4::class)
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+    @Test fun scroll_to_item() {
+
+    }
+}
